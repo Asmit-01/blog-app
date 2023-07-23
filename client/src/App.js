594c407c0abe80efff69891ab8ecf18c25fd1024
Asmit-01 <asmitkrpanika01@gmail.com>
2023-07-23 22:04:32 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Post from "./Post";
+import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
+import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
+
+function App() {
+  const deploy = 'http://localhost:4000/';
+
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage deploy={deploy} />} />
+          <Route path="/login" element={<LoginPage deploy={deploy} />} />
+          <Route path="/register" element={<RegisterPage deploy={deploy} />} />
+          <Route path="/create" element={<CreatePost deploy={deploy} />} />
+          <Route path="/post/:id" element={<PostPage deploy={deploy} />} />
+          <Route path="/edit/:id" element={<EditPost deploy={deploy} />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  );
+}
+
+export default App;
