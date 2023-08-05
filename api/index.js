a757b6896153d1e46c1bd2aeda84b5a87dec3228
Asmit-01 <asmitkrpanika01@gmail.com>
2023-08-05 12:39:13 +0530

    removed app.use(cors)

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ const upload = multer({ storage })
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
